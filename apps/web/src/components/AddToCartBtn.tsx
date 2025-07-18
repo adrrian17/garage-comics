8b8502ea152200886d15738276cd3bffbd832ffb
Adrian Ayala <adrian.ayala17@gmail.com>
2025-07-18 06:39:01 -0600

    Apply more linting

--- apps/web/src/components/AddToCartBtn.tsx
+++ apps/web/src/components/AddToCartBtn.tsx
@@ -3,25 +3,25 @@ import type { Price, Product } from "@/content/config";
 import { useCartStore } from "@/stores/cart-store";
 
 interface AddToCartBtnProps {
-	product: Product;
-	price: Price;
+  product: Product;
+  price: Price;
 }
 
 export function AddToCartBtn({ product, price }: AddToCartBtnProps) {
-	const addToCart = useCartStore((state) => state.addProduct);
-	const products = useCartStore((state) => state.line_items);
+  const addToCart = useCartStore((state) => state.addProduct);
+  const products = useCartStore((state) => state.line_items);
 
-	const isInCart = products.some((item) => item.productId === product.id);
+  const isInCart = products.some((item) => item.productId === product.id);
 
-	return (
-		<Button
-			className="w-full"
-			disabled={isInCart}
-			onClick={() => {
-				addToCart(product, price);
-			}}
-		>
-			{isInCart ? "En el carrito" : "Agregar al carrito"}
-		</Button>
-	);
+  return (
+    <Button
+      className="w-full"
+      disabled={isInCart}
+      onClick={() => {
+        addToCart(product, price);
+      }}
+    >
+      {isInCart ? "En el carrito" : "Agregar al carrito"}
+    </Button>
+  );
 }
--- apps/web/src/components/CartButton.tsx
+++ apps/web/src/components/CartButton.tsx
@@ -4,56 +4,56 @@ import { ShoppingCart } from "lucide-react";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
-	Sheet,
-	SheetContent,
-	SheetDescription,
-	SheetFooter,
-	SheetHeader,
-	SheetTitle,
-	SheetTrigger,
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
 } from "@/components/ui/sheet";
 import { useCartStore } from "@/stores/cart-store";
 
 export default function CartButton() {
-	const products = useCartStore((state) => state.line_items);
-	const total = useCartStore((state) => state.total);
+  const products = useCartStore((state) => state.line_items);
+  const total = useCartStore((state) => state.total);
 
-	const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-	const handleClick = async () => {};
+  const handleClick = async () => {};
 
-	return (
-		<Sheet>
-			<SheetTrigger asChild>
-				<Button>
-					<ShoppingCart />
-					{products.length}
-				</Button>
-			</SheetTrigger>
-			<SheetContent>
-				<SheetHeader>
-					<SheetTitle>Carrito</SheetTitle>
-					<SheetDescription hidden>Carrito</SheetDescription>
-				</SheetHeader>
-				{products?.map((item) => {
-					return (
-						<div key={item.productId}>
-							<span>{item.productName}</span>
-						</div>
-					);
-				})}
-				<SheetFooter>
-					<div className="flex flex-col">
-						<span>Total: ${total / 100}</span>
-						<Button
-							onClick={handleClick}
-							disabled={loading || products.length === 0}
-						>
-							{loading ? "Cargando..." : "Pagar"}
-						</Button>
-					</div>
-				</SheetFooter>
-			</SheetContent>
-		</Sheet>
-	);
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button>
+          <ShoppingCart />
+          {products.length}
+        </Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Carrito</SheetTitle>
+          <SheetDescription hidden>Carrito</SheetDescription>
+        </SheetHeader>
+        {products?.map((item) => {
+          return (
+            <div key={item.productId}>
+              <span>{item.productName}</span>
+            </div>
+          );
+        })}
+        <SheetFooter>
+          <div className="flex flex-col">
+            <span>Total: ${total / 100}</span>
+            <Button
+              onClick={handleClick}
+              disabled={loading || products.length === 0}
+            >
+              {loading ? "Cargando..." : "Pagar"}
+            </Button>
+          </div>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
 }
