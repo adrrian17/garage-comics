54bfda38ce553eea1a32b44ccbd0826766a5acd4
Adrian Ayala <adrian.ayala17@gmail.com>
2025-08-09 11:58:35 -0600

    Add a better design for the cart sheet

--- apps/web/src/stores/cart-store.ts
+++ apps/web/src/stores/cart-store.ts
@@ -7,10 +7,13 @@ type CartState = {
   line_items: {
     productId: string;
     priceId: string;
+    priceAmount: number;
     productName: string;
+    productImage?: string;
   }[];
   total: number;
   addProduct: (product: Product, price: Price) => void;
+  removeProduct: (productId: string) => void;
   clearCart: () => void;
 };
 
@@ -24,10 +27,28 @@ export const useCartStore = create<CartState>()(
           state.line_items.push({
             productId: product.data.id,
             priceId: price.data.id,
+            priceAmount: price.data.unit_amount,
             productName: product.data.name,
+            productImage: product.data.images?.[0],
           });
+
+          console.log(price);
+
           state.total += price.data.unit_amount;
         }),
+      removeProduct: (productId) =>
+        set((state) => {
+          const itemIndex = state.line_items.findIndex(
+            (item) => item.productId === productId,
+          );
+
+          if (itemIndex !== -1) {
+            const item = state.line_items[itemIndex];
+            state.total -= item.priceAmount;
+            state.line_items.splice(itemIndex, 1);
+          }
+        }),
+
       clearCart: () =>
         set((state) => {
           state.line_items = [];
