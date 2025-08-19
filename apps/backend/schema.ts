0045ecfe0528b22c62318a1951bdd5346b1e2ed1
Adrian Ayala <adrian.ayala17@gmail.com>
2025-08-19 08:30:33 -0600

    Add a confirmed and processed field to the orders table

--- apps/backend/schema.ts
+++ apps/backend/schema.ts
@@ -1,4 +1,4 @@
-import { index, jsonb, pgTable, serial } from "drizzle-orm/pg-core";
+import { boolean, index, jsonb, pgTable, serial } from "drizzle-orm/pg-core";
 import type { OrderMessage } from "./orders/orders";
 
 export const orders = pgTable(
@@ -6,6 +6,8 @@ export const orders = pgTable(
   {
     id: serial("id").primaryKey(),
     order: jsonb("message_payload").$type<OrderMessage>().notNull(),
+    processed: boolean("processed").notNull().default(false),
+    confirmed: boolean("confirmed").notNull().default(false),
   },
   (table) => [index("idx_message_payload_gin").using("gin", table.order)],
 );
