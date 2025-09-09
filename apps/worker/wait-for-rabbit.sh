0962e0ea63b3d115ce266189513f2fae7491ac78
Adrian Ayala <adrian.ayala17@gmail.com>
2025-09-09 07:54:42 -0600

    Add a bash script to wait for RabbitMQ

---
+++ apps/worker/wait-for-rabbit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+host="rabbitmq"
+port="5672"
+
+echo "⏳ Esperando a RabbitMQ en $host:$port..."
+
+while ! nc -z $host $port; do
+  sleep 2
+done
+
+echo "✅ RabbitMQ está listo, iniciando app..."
+exec npm start
